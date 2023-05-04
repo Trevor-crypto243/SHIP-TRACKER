a89be6aa69fa032c33962e625e40308f8ec7a911
Trevor Ogina <otitrevor450@gmail.com>
2023-05-04 13:04:20 +0300

    Progress bar
    Reprogrammed the progress bar

--- app/src/main/java/com/example/maps2/MapsActivity.kt
+++ app/src/main/java/com/example/maps2/MapsActivity.kt
@@ -11,10 +11,12 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
+import android.view.View.INVISIBLE
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ProgressBar
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -25,6 +27,7 @@ import java.io.InputStream
 class MapsActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var spinner: Spinner
+    private lateinit var progress_bar: ProgressBar
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
 
@@ -34,28 +37,9 @@ class MapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity4)
         supportActionBar?.hide()
 
-//        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//
-//        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-//            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-//            startActivity(settingsIntent)
-//        }
-//
-//        locationListener = object : LocationListener {
-//            override fun onLocationChanged(location: Location) {
-//                val latitude = location.latitude
-//                val longitude = location.longitude
-//                Toast.makeText(applicationContext, "lat: $latitude, long: $longitude", Toast.LENGTH_LONG).show()
-//            }
-//
-//            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-//            override fun onProviderEnabled(provider: String) {}
-//            override fun onProviderDisabled(provider: String) {}
-//        }
-
         webView = findViewById(R.id.webView)
         spinner = findViewById(R.id.spinner)
+        progress_bar = findViewById(R.id.progressBar)
 
         val websites = resources.getStringArray(R.array.ship_array)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, websites)
@@ -66,19 +50,12 @@ class MapsActivity : AppCompatActivity() {
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val selectedWebsite = parent?.getItemAtPosition(position).toString()
+
+                //get the gps location, use it as te center
+                //also consider the ships location
                 val myVariable = "Hello from Kotlin!"
-
                 loadWebsite("kotlinVariable", myVariable,position)
-
-//                if(position == 0){
-//                    loadWebsite("<html><body><h1>View 1, HTML!</h1></body></html>")
-//                }else if(position == 1){
-//                    loadWebsite("<html><body><h1>View 2, HTML!</h1></body></html>")
-//                }else if(position == 2){
-//                    loadWebsite("<html><body><h1>View 3, HTML!</h1></body></html>")
-//                }
             }
-
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 // Handle nothing selected
             }
@@ -97,58 +74,49 @@ class MapsActivity : AppCompatActivity() {
 
         webView.apply {
             webViewClient = object : WebViewClient() {
+                private var hasLoaded = false
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    if (!hasLoaded) {
+                        hasLoaded = true
+                        // Hide the progress bar or perform any other necessary actions
+                        progress_bar.visibility = View.INVISIBLE
+                    }
+                }
+
+                override fun onReceivedError(
+                    view: WebView?,
+                    errorCode: Int,
+                    description: String?,
+                    failingUrl: String?
+                ) {
+                    super.onReceivedError(view, errorCode, description, failingUrl)
+                    // Handle network error here
+                    Toast.makeText(applicationContext,""+errorCode,Toast.LENGTH_LONG).show()
+                }
+
                 override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                     view.loadUrl(url)
                     return true
                 }
             }
+
             settings.apply {
                 javaScriptEnabled = true
                 allowFileAccessFromFileURLs = true
                 allowUniversalAccessFromFileURLs = true
             }
-            if(offset == 0){
+
+            if (offset == 0) {
                 loadDataWithBaseURL(null, htmlContent1, "text/html", "UTF-8", null)
-            }else if(offset == 1){
+            } else if (offset == 1) {
                 loadDataWithBaseURL(null, htmlContent2, "text/html", "UTF-8", null)
-            }else if(offset == 2){
+            } else if (offset == 2) {
                 loadDataWithBaseURL(null, htmlContent3, "text/html", "UTF-8", null)
             }
         }
     }
-
-   
-
-
-
-
-//    override fun onStart() {
-//        super.onStart()
-//        if (ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.ACCESS_FINE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.ACCESS_COARSE_LOCATION
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION
-//                ),
-//                1
-//            )
-//            return
-//        }
-//        locationManager.requestLocationUpdates(
-//            LocationManager.GPS_PROVIDER,
-//            0,
-//            0f,
-//            locationListener
-//        )
-//    }
 
     override fun onStop() {
         super.onStop()
